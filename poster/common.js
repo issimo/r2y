258d2d92b058d9b90c35749e43e6236eec6e2d41
Andrew Olowude <aolowude@gmail.com>
2015-11-11 18:21:24 +0100

    Code Input for DB

---
+++ poster/common.js
@@ -0,0 +1,10 @@
+Router.route('/',{name:'poster'});
+
+
+imageStore = new FS.Store.GridFS("images", {
+    //Put in optional parameters
+});
+
+Images = new FS.Collection("images" , {
+    stores: [imageStore]
+});
--- poster/poster.js
+++ poster/poster.js
@@ -1,25 +1,34 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.body.events({
+        'change .myFileInput' : function(event, template) {
+            alert("Clicked");
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+            //var image = event.target.files;   
+            FS.Utility.eachFile(event, function(file) {
+                Images.insert(file, function(err, fileObj) {
+                   //New doc with ID: fileObj._id and sent off the data upload using HTTP 
+                    alert("Uploaded");
+                });
+            });
+        }
+    });
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
     // code to run on server at startup
-  });
+    });
+
+    Images.allow({
+        'insert': function () {
+        // add custom authentication code here
+        return true;
+        }
+    });
+
 }
 
 Router.configure({

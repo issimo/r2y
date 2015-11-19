ba2d9dd68b5f9e2a5135d8f98e863b3bb22d83d9
Andrew Olowude <aolowude@gmail.com>
2015-11-19 14:40:26 +0100

    Images now saved on Cloudinary

--- poster/client/views/postcard/postcard.js
+++ poster/client/views/postcard/postcard.js
@@ -1,9 +1,13 @@
 Template.postcard.events({
 	'click .deletePoster':function(){
-		Images.remove(this._id);
+		Posters.remove(this._id);
+		console.log(this._id);
+		/*
 		Cloudinary.delete @response.public_id, (err,res) ->
             console.log "Upload Error: #{err}"
-            console.log "Upload Result: #{res}"	}
+            console.log "Upload Result: #{res}"
+        */
+    }
 });
 
 Meteor.call("cloudinary_delete","public_id",function(e,r){

e708c3103e0c78dc2ee2ffff8f5b4949ca7a3263
Andrew Olowude <aolowude@gmail.com>
2015-11-20 13:04:24 +0100

    Fixed issue

--- poster/client/views/postcard/postcard.js
+++ poster/client/views/postcard/postcard.js
@@ -1,5 +1,6 @@
 Template.postcard.events({
 	'click .deletePoster':function(){
+
 		Posters.remove(this._id);
         Meteor.call("c.delete_by_public_id","public_id",function(e,r){
             if(!e){
@@ -11,11 +12,6 @@ Template.postcard.events({
             }
         });
 		console.log(this.imageId);
-		/*
-		Cloudinary.delete @response.public_id, (err,res) ->
-            console.log "Upload Error: #{err}"
-            console.log "Upload Result: #{res}"
-        */
-    }
+		
 });
 

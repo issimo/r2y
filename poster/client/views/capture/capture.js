59f21601e33ec4aff6a1a6663a41d9e222339a61
Andrew Olowude <aolowude@gmail.com>
2015-11-20 22:25:52 +0100

    I do

--- poster/client/views/capture/capture.js
+++ poster/client/views/capture/capture.js
@@ -1,21 +1,24 @@
 Template.capture.events({
     'change .myFileInput' : function(event,template) {
+        alert("1. In here");
 
          FS.Utility.eachFile(event, function(file) {
+            alert(file.name);
+            console.log(file.size);
+
           Images.insert(file, function (err, fileObj) {
           if (err){
              // handle error
-             console.log("Error"+err);
+             alert("Error"+err);
           } else {
              // handle success depending what you need to do
             var userId = Meteor.userId();
-            var imagesURL = {
-              "relLink": "/cfs/files/images/" + fileObj._id
-            };
-            Session.set('imgUrl',imagesURL)
+            var imagesURL = { "relLink": "/cfs/files/images/" + fileObj._id };
+            alert("3. Around here");
+
+            Session.set('imgUrl',imagesURL);
             alert(imagesURL.relLink);
             //Meteor.users.update(userId, {$set: imagesURL});
-
           }
         });
           //Router.go('confirm');

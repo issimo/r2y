407e01287c18c57c7549dd54fb4fee9fb7bdb1d8
Andrew Olowude <aolowude@gmail.com>
2015-11-22 17:26:55 +0100

    SkRT

--- poster/client/views/confirm/confirm.js
+++ poster/client/views/confirm/confirm.js
@@ -46,7 +46,9 @@ Template.confirm.events({
             $(".delete,.accept,.retake").hide();
             //insert iphone image css rotate stuff here
             var ioS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-            if(ioS==true){alert('isIos!!!')$('#userImage').addClass('rotate90R');}
+            if(ioS==true){
+                alert('isIos!!!');
+                $('#userImage').addClass('rotate90R');}
             html2canvas($('.posIm'), {
              height:515,
             width:365,
